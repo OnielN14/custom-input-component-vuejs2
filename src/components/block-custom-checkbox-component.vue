f57895c0b25eff4381711c59501ed8fcd72144c7
Daniyal Ahmad Rizaldhi <onieln14@gmail.com>
2019-02-03 14:53:51 +0700

    removing dev code

--- src/components/block-custom-checkbox-component.vue
+++ src/components/block-custom-checkbox-component.vue
@@ -3,10 +3,6 @@
     <h3>{{blockTitle}}</h3>
 
     <checkbox v-for="(item,index) in checkboxData.data" :key="index" :nameGroup="checkboxData.name" :values="item.value" :label="item.label" :id="checkboxData.name+index" v-model="selected" :modelValue="selected" @input="updateValue"></checkbox>
-
-    <div class="">
-      hoby : {{selected}}
-    </div>
   </section>
 </template>
 
